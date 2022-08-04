34e4e1e21d9340143933f936bb554288da59557b
ivan <vivan.volovik@gmail.com>
2022-08-04 11:29:11 +0300

    add deposit

---
+++ deploy/deposit.js
@@ -0,0 +1,54 @@
+// process.exit(0); // Block file in order to not accidentally deploy
+
+const { ethers } = require("hardhat");
+const { utils } = ethers;
+const { BigNumber } = require("ethers");
+
+const deposit = async () => {
+    let MyContract = await ethers.getContractFactory("Vault");
+    const Vault = await MyContract.attach("0x6894cf73D22B34fA2b30E5a4c706AD6c2f2b24ac");
+    let tx;
+
+    let tS = (await Vault.totalSupply()).toString();
+
+    let eth = utils.parseUnits("0.03", 18); //50
+    let usdc = utils.parseUnits("25", 6);
+    let sqth = utils.parseUnits("0.16", 18);
+
+    let data = await Vault.calcSharesAndAmounts(eth, usdc, sqth, tS);
+    console.log(data);
+
+    console.log(eth.toString());
+    console.log(data[1].toString());
+    console.log(usdc.toString());
+    console.log(data[2].toString());
+    console.log(sqth.toString());
+    console.log(data[3].toString());
+
+    // let WETH = await ethers.getContractAt("IWETH", "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2");
+    // let USDC = await ethers.getContractAt("IWETH", "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48");
+    // let OSQTH = await ethers.getContractAt("IWETH", "0xf1B99e3E573A1a9C5E6B2Ce818b617F0E664E86B");
+
+    // const maxApproval = BigNumber.from(
+    //     "115792089237316195423570985008687907853269984665640564039457584007913129639935"
+    // );
+    // tx = await WETH.approve("0x6894cf73D22B34fA2b30E5a4c706AD6c2f2b24ac", maxApproval);
+
+    // tx = await USDC.approve("0x6894cf73D22B34fA2b30E5a4c706AD6c2f2b24ac", maxApproval);
+
+    // tx = await OSQTH.approve("0x6894cf73D22B34fA2b30E5a4c706AD6c2f2b24ac", maxApproval);
+
+    let to = "0x42B1299fCcA091A83C08C24915Be6E6d63906b1a";
+
+    tx = await Vault.deposit(eth, usdc, sqth, to, 0, 0, 0, {
+        gasPrice: 8000000000,
+        gasLimit: 900000,
+        nonce: 24,
+    });
+    await tx.wait();
+};
+
+deposit().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
--- deploy/populate.js
+++ deploy/populate.js
@@ -5,8 +5,9 @@ const { ethers } = require("hardhat");
 const populate = async () => {
     // const Rebalancer = await ethers.getContractAt("Rebalancer", "0xD3ed5915AAA27dB7a3646bf926dB6C98243d5c40");
     const MyContract = await ethers.getContractFactory("Rebalancer");
-    const Rebalancer = await MyContract.attach("0xD3ed5915AAA27dB7a3646bf926dB6C98243d5c40");
-    let tx;
+    const Rebalancer = await MyContract.attach("0x09b1937D89646b7745377f0fcc8604c179c06aF5");
+
+    // let tx;
     // tx = await Rebalancer.setContracts(
     //     "0xA9a68eA2746793F43af0f827EC3DbBb049359067",
     //     "0xfbcF638ea33A5F87D1e39509E7deF653958FA9C4",
@@ -18,7 +19,7 @@ const populate = async () => {
     // );
     // await tx.wait();
 
-    console.log(await Rebalancer.addressMath());
+    console.log(await Rebalancer.addressAuction());
 };
 
 populate().catch((error) => {
