691f56ca9314ab9543d67838d44c4852483a43ae
leooos33 <sigmaw39@gmail.com>
2022-08-16 23:20:57 +0300

    20

--- test/20 - snapshot.test.js
+++ test/20 - snapshot.test.js
@@ -1,12 +1,12 @@
 const { ethers } = require("hardhat");
 const { utils } = ethers;
-const { wethAddress, osqthAddress, usdcAddress, _governanceAddress, _vaultStorageAddress } = require("./common");
+const { wethAddress, osqthAddress, usdcAddress, _governanceAddress, _vaultStorageAddress, _vaultMathAddress } = require("./common");
 const { resetFork, getERC20Balance, approveERC20 } = require("./helpers");
 const { deployContract } = require("./deploy");
 
 describe("Snapshot", function () {
     it("Get snapshot", async function () {
-        await resetFork(15345193);
+        await resetFork(15354389);
 
         const VaultStorage = await ethers.getContractAt("VaultStorage", _vaultStorageAddress);
 
@@ -27,5 +27,18 @@ describe("Snapshot", function () {
         console.log("ivAtLastRebalance:", ivAtLastRebalance.toString());
         console.log("totalValue:", totalValue.toString());
         console.log("ethPriceAtLastRebalance:", ethPriceAtLastRebalance.toString());
+
+        const VaultMath = await ethers.getContractAt("VaultMath", _vaultMathAddress);
+
+        const prices = await VaultMath.getPrices();
+        console.log("ETH/USDC price %s", prices[0]);
+        console.log("oSQTH/ETH price %s", prices[1]);
+        const amounts = await VaultMath.getTotalAmounts();
+        console.log("wETH amount %s", amounts[0]);
+        console.log("USDC amount %s", amounts[1]);
+        console.log("oSQTH amount %s", amounts[2]);
+
+        const value = await VaultMath.getValue(amounts[0], amounts[1], amounts[2], prices[0], prices[1]);
+        console.log("Total ETH value %s", value);
     });
 });
