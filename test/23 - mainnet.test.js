91e61c0be59032d672fb654e63ff1c3d5a64bd2e
leooos33 <sigmaw39@gmail.com>
2022-08-22 16:21:12 +0300

    wip

--- test/23 - mainnet.test.js
+++ test/23 - mainnet.test.js
@@ -7,14 +7,15 @@ const {
     _vaultAuctionAddress,
     _governanceAddress,
     _vaultStorageAddress,
+    maxUint256
 } = require("./common");
 const { resetFork, getERC20Balance } = require("./helpers");
+const { BigNumber } = require("ethers");
 
 describe.only("Rebalance test mainnet", function () {
-    let tx, receipt, MyContract;
+    let tx, receipt, MyContract, governance;
     // let actor;
     // let actorAddress = _governanceAddress;
-    let governance;
 
     it("Should deploy contract", async function () {
         await resetFork(15389324);
@@ -32,6 +33,8 @@ describe.only("Rebalance test mainnet", function () {
 
         governance = await ethers.getSigner(_governanceAddress);
         console.log("governance:", governance.address);
+        console.log("auction:", VaultAuction.address);
+
 
         // const signers = await ethers.getSigners();
         // chad = signers[0];
@@ -43,13 +46,16 @@ describe.only("Rebalance test mainnet", function () {
     });
 
     it("rebalance with flash loan", async function () {
+        let governanceAddress = _governanceAddress;
+        governance = await ethers.getSigner(governanceAddress);
+
         // Comment here to test pause
         tx = await VaultStorage.connect(governance).setPause(false, {
             gasLimit: 40000,
             gasPrice: 11000000000,
         });
 
-        tx = await VaultStorage.connect(governance).setMinPriceMultiplier(0, {
+        tx = await VaultStorage.connect(governance).setMinPriceMultiplier(utils.parseUnits("1", 16), {
             gasLimit: 40000,
             gasPrice: 11000000000,
         });
@@ -61,9 +67,40 @@ describe.only("Rebalance test mainnet", function () {
         });
         await tx.wait();
 
+        tx = await VaultStorage.connect(governance).setBaseThreshold(100000, {
+            gasLimit: 40000,
+            gasPrice: 11000000000,
+        });
+        await tx.wait();
+
         console.log("> actor WETH %s", await getERC20Balance(governance.address, wethAddress));
         console.log("> actor USDC %s", await getERC20Balance(governance.address, usdcAddress));
         console.log("> actor oSQTH %s", await getERC20Balance(governance.address, osqthAddress));
+
+        let WETH = await ethers.getContractAt("IWETH", wethAddress);
+        let USDC = await ethers.getContractAt("IWETH", usdcAddress);
+        let OSQTH = await ethers.getContractAt("IWETH", osqthAddress);
+        const maxApproval = BigNumber.from(maxUint256);
+
+        console.log("params %s", await VaultAuction.getAuctionParams(1661070257));
+
+        //dalsche ne rabotaet
+        tx = await WETH.connect(governance).approve(_vaultAuctionAddress, maxApproval, {
+            gasLimit: 50000,
+            gasPrice: 11000000000,
+        });
+        await tx.wait();
+        tx = await USDC.connect(governance).approve(_vaultAuctionAddress, maxApproval, {
+            gasLimit: 50000,
+            gasPrice: 11000000000,
+        });
+        await tx.wait();
+        tx = await OSQTH.connect(governance).approve(_vaultAuctionAddress, maxApproval, {
+            gasLimit: 50000,
+            gasPrice: 11000000000,
+        });
+        await tx.wait();
+        console.log("here");
 
         tx = await VaultAuction.connect(governance).timeRebalance(governance.address, 0, 0, 0, {
             gasLimit: 2500000,
