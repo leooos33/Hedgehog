df9f366e804ec01543794e3b595130da93a57928
ivan <vivan.volovik@gmail.com>
2022-08-22 19:51:26 +0300

    +

--- test/23 - mainnet.test.js
+++ test/23 - mainnet.test.js
@@ -8,15 +8,12 @@ const {
     _governanceAddress,
     _vaultStorageAddress,
     maxUint256,
-    _rebalancerBigAddress
+    _rebalancerBigAddress,
 } = require("./common");
 const { resetFork, getERC20Balance, getERC20Allowance, approveERC20 } = require("./helpers");
-const { BigNumber } = require("ethers");
 
 describe.only("Rebalance test mainnet", function () {
     let tx, receipt, MyContract, governance, Rebalancer;
-    // let actor;
-    // let actorAddress = _governanceAddress;
 
     it("Should deploy contract", async function () {
         await resetFork(15389324);
@@ -26,8 +23,9 @@ describe.only("Rebalance test mainnet", function () {
 
         MyContract = await ethers.getContractFactory("VaultStorage");
         VaultStorage = await MyContract.attach(_vaultStorageAddress);
-        Rebalancer = await MyContract.attach(_rebalancerBigAddress);
 
+        MyContract = await ethers.getContractFactory("BigRebalancer");
+        Rebalancer = await MyContract.attach(_rebalancerBigAddress);
 
         await hre.network.provider.request({
             method: "hardhat_impersonateAccount",
@@ -38,19 +36,15 @@ describe.only("Rebalance test mainnet", function () {
         console.log("governance:", governance.address);
         console.log("auction:", VaultAuction.address);
 
-        const signers = await ethers.getSigners();
-        chad = signers[0];
-
-        await chad.sendTransaction({
-            to: governance.address,
-            value: ethers.utils.parseEther("5.0"),
-        });
+        // const signers = await ethers.getSigners();
+        // chad = signers[0];
+        // await chad.sendTransaction({
+        //     to: governance.address,
+        //     value: ethers.utils.parseEther("5.0"),
+        // });
     });
 
     it("rebalance with flash loan", async function () {
-        let governanceAddress = _governanceAddress;
-        governance = await ethers.getSigner(governanceAddress);
-
         // Comment here to test pause
         tx = await VaultStorage.connect(governance).setPause(false, {
             gasLimit: 40000,
@@ -63,11 +57,11 @@ describe.only("Rebalance test mainnet", function () {
         // });
         // await tx.wait();
 
-         tx = await VaultStorage.connect(governance).setRebalanceThreshold(utils.parseUnits("1", 18), {
-             gasLimit: 40000,
-             gasPrice: 11000000000,
-         });
-         await tx.wait();
+        tx = await VaultStorage.connect(governance).setRebalanceThreshold(utils.parseUnits("1", 18), {
+            gasLimit: 40000,
+            gasPrice: 11000000000,
+        });
+        await tx.wait();
 
         //  tx = await VaultStorage.connect(governance).setBaseThreshold(100000, {
         //      gasLimit: 40000,
@@ -75,23 +69,23 @@ describe.only("Rebalance test mainnet", function () {
         //  });
         //  await tx.wait();
 
-          tx = await VaultStorage.connect(governance).setProtocolFee(1, {
-              gasLimit: 50000,
-              gasPrice: 11000000000,
-          });
-          await tx.wait();
+        tx = await VaultStorage.connect(governance).setProtocolFee(1, {
+            gasLimit: 50000,
+            gasPrice: 11000000000,
+        });
+        await tx.wait();
 
         console.log("> actor WETH %s", await getERC20Balance(governance.address, wethAddress));
         console.log("> actor USDC %s", await getERC20Balance(governance.address, usdcAddress));
         console.log("> actor oSQTH %s", await getERC20Balance(governance.address, osqthAddress));
 
-        await approveERC20(governance, _vaultAuctionAddress, maxUint256, wethAddress);
-        await approveERC20(governance, _vaultAuctionAddress, maxUint256, usdcAddress);
-        await approveERC20(governance, _vaultAuctionAddress, maxUint256, osqthAddress);
+        // await approveERC20(governance, _vaultAuctionAddress, maxUint256, wethAddress);
+        // await approveERC20(governance, _vaultAuctionAddress, maxUint256, usdcAddress);
+        // await approveERC20(governance, _vaultAuctionAddress, maxUint256, osqthAddress);
 
-        console.log("> actor WETH %s", await getERC20Allowance(governance.address, _vaultAuctionAddress, wethAddress));
-        console.log("> actor USDC %s", await getERC20Allowance(governance.address, _vaultAuctionAddress, usdcAddress));
-        console.log("> actor oSQT %s", await getERC20Allowance(governance.address, _vaultAuctionAddress, osqthAddress));
+        // console.log("> actor WETH %s", await getERC20Allowance(governance.address, _vaultAuctionAddress, wethAddress));
+        // console.log("> actor USDC %s", await getERC20Allowance(governance.address, _vaultAuctionAddress, usdcAddress));
+        // console.log("> actor oSQT %s", await getERC20Allowance(governance.address, _vaultAuctionAddress, osqthAddress));
 
         console.log("params %s", await VaultAuction.getAuctionParams(1661070257));
 
@@ -100,12 +94,13 @@ describe.only("Rebalance test mainnet", function () {
             // gas: 1800000,
             gasPrice: 23000000000,
         });
-
-        tx = await VaultAuction.connect(governance).timeRebalance(governance.address, 0, 0, 0, {
-            gasLimit: 2500000,
-            gasPrice: 11000000000,
-        });
         receipt = await tx.wait();
+
+        // tx = await VaultAuction.connect(governance).timeRebalance(governance.address, 0, 0, 0, {
+        //     gasLimit: 2500000,
+        //     gasPrice: 11000000000,
+        // });
+        // receipt = await tx.wait();
 
         console.log("> Gas used withdraw + fl: %s", receipt.gasUsed);
 
