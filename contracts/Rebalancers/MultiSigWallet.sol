7b4e3f6bb2ee071ca5c5370caf533aafd2206618
ivan <vivan.volovik@gmail.com>
2023-01-20 15:40:40 +0200

    hardcode addresses

--- contracts/Rebalancers/MultiSigWallet.sol
+++ contracts/Rebalancers/MultiSigWallet.sol
@@ -16,7 +16,7 @@ contract MultiSigWallet {
 
     address[] public owners;
     mapping(address => bool) public isOwner;
-    uint public numConfirmationsRequired;
+    uint public numConfirmationsRequired = 2;
 
     struct Transaction {
         address to;
@@ -51,25 +51,13 @@ contract MultiSigWallet {
         _;
     }
 
-    constructor(address[] memory _owners, uint _numConfirmationsRequired) {
-        require(_owners.length > 0, "owners required");
-        require(
-            _numConfirmationsRequired > 0 &&
-                _numConfirmationsRequired <= _owners.length,
-            "invalid number of required confirmations"
-        );
-
-        for (uint i = 0; i < _owners.length; i++) {
-            address owner = _owners[i];
-
-            require(owner != address(0), "invalid owner");
-            require(!isOwner[owner], "owner not unique");
-
-            isOwner[owner] = true;
-            owners.push(owner);
-        }
-
-        numConfirmationsRequired = _numConfirmationsRequired;
+    constructor(address[] memory _owners) {
+        isOwner[0x4530DA167C5a751e48f35b2aa08F44570C03B7dd] = true;
+        owners.push(0x4530DA167C5a751e48f35b2aa08F44570C03B7dd);
+        isOwner[0x4530DA167C5a751e48f35b2aa08F44570C03B7dd] = true;
+        owners.push(0x4530DA167C5a751e48f35b2aa08F44570C03B7dd);
+        isOwner[0x4530DA167C5a751e48f35b2aa08F44570C03B7dd] = true;
+        owners.push(0x4530DA167C5a751e48f35b2aa08F44570C03B7dd);
     }
 
     receive() external payable {
